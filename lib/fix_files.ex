b5046713211a220d2b17ed60a25d04adc03768ba
Cort Fritz <cortfritz@Corts-MacBook-Pro.local>
2022-11-23 12:58:28 -0800

    mix new

---
+++ lib/fix_files.ex
@@ -0,0 +1,18 @@
+defmodule FixFiles do
+  @moduledoc """
+  Documentation for `FixFiles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FixFiles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fix_files_test.exs
@@ -0,0 +1,8 @@
+defmodule FixFilesTest do
+  use ExUnit.Case
+  doctest FixFiles
+
+  test "greets the world" do
+    assert FixFiles.hello() == :world
+  end
+end
